9e90cd09e9408bd45c3f82a46a1182ba53bb5a58
j-hnn <john.weng@searcyschools.org>
2024-03-05 11:12:33 -0600

    added pathfinding to enemy
    started work on player gui

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 
-@export var speed = 300.0
+@export var speed = 300
+
+var dir : Vector2
 
 
-func _physics_process(delta):
-	var move_input = Input.get_axis("ui_down", "ui_up")
-	var rotation_direction = Input.get_axis("ui_left", "ui_right")
-
-	velocity = transform.x * move_input * speed
-	rotation += rotation_direction * delta * 2.5
+func _physics_process(_delta):
+	velocity = dir * speed
 	move_and_slide()
 	
+func _unhandled_input(_event):
+	dir.x = Input.get_axis("ui_left", "ui_right")
+	dir.y = Input.get_axis("ui_up", "ui_down")
+	dir = dir.normalized()
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-func _input(event):
-	if event is InputEventMouseButton:
-		var tween = create_tween()
-		tween.tween_property(self, "position", event.position, 1)
+#func _input(event):
+#	if event is InputEventMouseButton:
+	#	var tween = create_tween()
+		#tween.tween_property(self, "position", event.position, 1)
